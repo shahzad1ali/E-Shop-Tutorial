e060e47b56414478ff80284c17d047cc80a36c2b
shahzad1ali <shahzadmobi619@gmail.com>
2025-09-16 20:33:50 +0500

    update DB & server file

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,7 +1,6 @@
  export const server = 'https://e-shop-tutorial-psi.vercel.app/api/v2';
- export const backend_url = 'http://localhost:8000';
  
-// export const backend_url = 'https://e-shop-tutorial-psi.vercel.app';
+export const backend_url = 'https://e-shop-tutorial-psi.vercel.app';
 
 //  export const server = 'http://localhost:8000/api/v2';
 
