3c69485120c0b194b9a263cbf870f12d277dfd49
shahzad1ali <shahzadmobi619@gmail.com>
2025-09-16 11:51:33 +0500

    update DB & server file

--- backend/db/dataBase.js
+++ backend/db/dataBase.js
@@ -33,34 +33,63 @@
 
 
 
+// const mongoose = require("mongoose");
+// mongoose.set("strictPopulate", false);
+// const connectDbWithRetry = async (dbURI, maxRetries) => {
+//   console.log("🚀 ~ connectDbWithRetry ~ maxRetries:", maxRetries)
+//   console.log("🚀 ~ connectDbWithRetry ~ dbURI:", dbURI)
+//   for (let retries = 0; retries < maxRetries; retries++) {
+//     try {
+//       const uri = dbURI;
+//       await mongoose.connect(uri, {
+//         dbName: "e-commerce",
+//         connectTimeoutMS: 20000,
+//       });
+//       console.log("Connected to MongoDB");
+
+//       break;
+//     } catch (err) {
+//       console.log("Failed to connect to MongoDB: ${err}. Retrying...",err);
+//       if (retries === maxRetries) {
+//         console.error("Max connection retries reached.");
+//         break;
+//       }
+//       await new Promise((resolve) => setTimeout(resolve, 5000));
+//     }
+//   }
+// };
+
+//  const connectDB = async () => {
+//   await connectDbWithRetry(process.env.DB_URL, 2);
+// };
+
+// module.exports = connectDB;
+
+
+
 const mongoose = require("mongoose");
-mongoose.set("strictPopulate", false);
+
 const connectDbWithRetry = async (dbURI, maxRetries) => {
-  console.log("🚀 ~ connectDbWithRetry ~ maxRetries:", maxRetries)
-  console.log("🚀 ~ connectDbWithRetry ~ dbURI:", dbURI)
   for (let retries = 0; retries < maxRetries; retries++) {
     try {
-      const uri = dbURI;
-      await mongoose.connect(uri, {
-        dbName: "e-commerce",
+      await mongoose.connect(dbURI, {
         connectTimeoutMS: 20000,
       });
-      console.log("Connected to MongoDB");
-
+      console.log("✅ Connected to MongoDB");
       break;
     } catch (err) {
-      console.log("Failed to connect to MongoDB: ${err}. Retrying...",err);
-      if (retries === maxRetries) {
-        console.error("Max connection retries reached.");
-        break;
+      console.log("❌ Failed to connect:", err.message);
+      if (retries === maxRetries - 1) {
+        console.error("Max retries reached.");
+        process.exit(1);
       }
       await new Promise((resolve) => setTimeout(resolve, 5000));
     }
   }
 };
 
- const connectDB = async () => {
-  await connectDbWithRetry(process.env.DB_URL, 2);
+const connectDB = async () => {
+  await connectDbWithRetry(process.env.DB_URL, 5);
 };
 
 module.exports = connectDB;
